cffd86ccf3f19b448465eb9c56ecccb7c672e43a
Amr <amrworkfront@gmail.com>
2026-01-15 19:43:53 +0200

    Revert "Refactor cookie settings for access and refresh tokens to improve security and consistency"
    This reverts commit 9ed66989f8d0b2a0240bc3d89fafe6a39dfff954.

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Trust Proxy (Required for Vercel/Railway Proxy)
-app.set('trust proxy', 1);
-
 // Security Middleware
 app.use(helmet());
 
